0910f3f8b7a970b58339b540f0753ff021b89343
babykittenz <kidbymichaelb@gmail.com>
2025-02-12 12:23:14 -0600

    fix: cover gotchas in routines

--- links.go
+++ links.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"time"
 )
-
-var wg sync.WaitGroup
 
 var links = []string{
 	"http://google.com",
@@ -16,12 +14,29 @@ var links = []string{
 	"http://amazon.com",
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
+}
+
+func checkMultipleLinks() {
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
 func main() {
-
-	for _, link := range links {
-		wg.Add(1) // Increment the WaitGroup counter
-		go func(l string) {
-			defer wg.Done() // Decrement counter when goroutine finishes
-			checkLink(l)
-		}(link)
-	}
-
-	wg.Wait() // Wait for all goroutines to finish before exiting
+	checkMultipleLinks()
 }
